bb28c5e6de23ce14cc2c49ac2029eefa89bb8ffd
hwansoo17 <playground99@naver.com>
2024-01-22 14:57:29 +0900

    Add: 앱 실행시 엑세스토큰 유효성 확인과정 추가

--- App.js
+++ App.js
@@ -10,6 +10,7 @@ import MeetingStackScreen from './Screens/StackScreens/MeetingStackScreen';
 import ChattingStackScreen from './Screens/StackScreens/ChattingStackScreen';
 import messaging from '@react-native-firebase/messaging';
 import pushNoti from "./pushNoti";
+import config from './config';
 
 messaging().setBackgroundMessageHandler(async remoteMessage => {
   console.log('[백그라운드에서 수신한 메시지]', remoteMessage);
@@ -45,15 +46,54 @@ function BottomTabNavigator() {
 const App = () => {
   const [initialRoute, setInitialRoute] = useState(null);
 
-  useEffect(() => {
-    AsyncStorage.getItem('token')
-    .then((value) => {
-      if (value) {
+  const checkLoginStatus = async () => {
+    const email = await AsyncStorage.getItem('email');
+    const accessToken = await AsyncStorage.getItem('accessToken');
+    const refreshToken = await AsyncStorage.getItem('refreshToken');
+    //엑세스 토큰이 있으면 토큰 유효성 검사
+    console.log('액세스토큰'+accessToken);
+    if (accessToken) {
+      const response = await fetch(config.SERVER_URL+'/token/checkToken', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer '+accessToken
+        }
+      });
+      console.log('액세스토큰 유효성 여부'+response.status);
+      //토큰이 유효하면 이니셜 라우트를 홈으로 설정(자동 로그인)
+      if (response.status == 200) {
         setInitialRoute('Root');
       } else {
-        setInitialRoute('Auth');
-      }
-    })
+        //토큰이 유효하지 않으면 토큰 재발급 시도
+        const response = await fetch(config.SERVER_URL+'/token/re-issue', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({email: email, refreshToken: refreshToken})
+        });
+        console.log('토큰재발급 성공 여부'+response.status);
+        //토큰 재발급 성공하면 토큰 저장하고 함수 재실행
+        if (response.status == 200) {
+          const data = await response.json();
+          await AsyncStorage.setItem('accessToken', data.accessToken);
+          await AsyncStorage.setItem('refreshToken', data.refreshToken);
+          checkLoginStatus();
+        } else {
+          //토큰 재발급 실패하면 로그인 화면으로 이동
+          console.log('토큰 재발급 실패');
+          setInitialRoute('Auth');
+        };
+      };
+    } else {
+      //엑세스 토큰이 없으면 로그인 화면으로 이동
+      console.log('엑세스 토큰 없음');
+      setInitialRoute('Auth');
+    };
+  };
+  useEffect(() => {
+    checkLoginStatus();
   }, []);
 
   useEffect(() => {
@@ -68,7 +108,7 @@ const App = () => {
     const getFcmToken = async () => {
       const fcmToken = await messaging().getToken();
       await AsyncStorage.setItem('fcmToken', fcmToken);
-      console.log(fcmToken);
+      console.log('fcm토큰'+fcmToken);
     };
 
     getFcmToken();
--- config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    SERVER_URL: 'http://52.78.219.76:8080',
-};
-
-export default config;
