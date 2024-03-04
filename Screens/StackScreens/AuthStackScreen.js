f59f8f053c15e34b18939cf9a555797b41763950
hwansoo17 <playground99@naver.com>
2024-03-04 16:54:32 +0900

    Fix: 화면 이름 변경

--- Screens/StackScreens/AuthStackScreen.js
+++ Screens/StackScreens/AuthStackScreen.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import LoginScreen from '../AuthScreens/LoginScreen';
-import RegisterScreen from '../AuthScreens/RegisterScreen';
-import EmailAuthScreen from '../AuthScreens/EmailAuthScreen';
-import PasswordResetScreen from '../AuthScreens/PasswordResetScreen';
+import Login from '../AuthScreens/Login';
+import Register1 from '../AuthScreens/Register1';
+import Register2 from '../AuthScreens/Register2';
+import PasswordReset1 from '../AuthScreens/PasswordReset1';
+import PasswordReset2 from '../AuthScreens/PasswordReset2';
 const Stack = createNativeStackNavigator();
 
 
@@ -11,10 +12,11 @@ const Stack = createNativeStackNavigator();
 const AuthStackScreen = () => {
   return (
     <Stack.Navigator initialRouteName='Login'>
-      <Stack.Screen name="Login" component={LoginScreen}/>
-      <Stack.Screen name="EmailAuth" component={EmailAuthScreen}/>
-      <Stack.Screen name="Register" component={RegisterScreen}/>
-      <Stack.Screen name="PasswordReset" component={PasswordResetScreen}/>
+      <Stack.Screen name="Login" component={Login}/>
+      <Stack.Screen name="Register1" component={Register1}/>
+      <Stack.Screen name="Register2" component={Register2}/>
+      <Stack.Screen name="PasswordReset1" component={PasswordReset1}/>
+      <Stack.Screen name="PasswordReset2" component={PasswordReset2}/>
     </Stack.Navigator>
   )
 };
