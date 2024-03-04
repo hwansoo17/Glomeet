fff96d93bc2df562325aba5769f9bc563b97f8c0
hwansoo17 <playground99@naver.com>
2024-03-04 16:50:35 +0900

    Add: 모임 화면 초기 구현

--- Screens/MeetingScreens/MeetingDetailScreen.js
+++ Screens/MeetingScreens/MeetingDetailScreen.js
@@ -2,16 +2,27 @@ import React from "react";
 import {View, Text, TouchableOpacity} from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const MeetingDetailScreen = ({navigation}) => {
-    
-    return (
-        <View>
-            <Text>MeetingDetailScreen</Text>
-            <TouchableOpacity>
-                <Text></Text>
-            </TouchableOpacity>
-        </View>
-    )
-    };
+const MeetingDetailScreen = ({route, navigation}) => {
+  const meeting = route.params.meeting;
+  return (
+    <View>
+      <Text>{meeting.name}</Text>
+      <Text>{meeting.description}</Text>
+      <View style={{flexDirection:'row'}}>
+        <Text>{meeting.date}</Text>
+        <View style={{flex:1}}/>
+        <Text>{meeting.memberCount}</Text>      
+      </View>
+      <View style={{flexDirection:'row'}}>
+        <Text>{meeting.keyword}</Text>
+        <View style={{flex:1}}/>
+        <Text>{meeting.place}</Text>      
+      </View>
+      <TouchableOpacity>
+          <Text>모임 참여하기</Text>
+      </TouchableOpacity>
+    </View>
+  )
+};
 
 export default MeetingDetailScreen;
