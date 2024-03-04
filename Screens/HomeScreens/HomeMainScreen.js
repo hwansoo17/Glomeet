e599e77811cc62cc84aa915558aabd3cda3bfaa8
hwansoo17 <playground99@naver.com>
2024-03-04 16:48:32 +0900

    Fix: 로그아웃시 로그인 화면이동 로직 수정

--- Screens/HomeScreens/HomeMainScreen.js
+++ Screens/HomeScreens/HomeMainScreen.js
@@ -3,29 +3,25 @@ import {View, Text, TouchableOpacity} from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import config from '../../config';
 import { useWebSocket } from "../../WebSocketProvider";
-
+import { api } from '../../api'
 const HomeMainScreen = ({navigation}) => {
   const webSocketClient = useWebSocket();
 
   const loggedOut = async () => {
     const email = await AsyncStorage.getItem('email');
     const fcmToken = await AsyncStorage.getItem('fcmToken');
-    const response = await fetch(config.SERVER_URL+'/auth/signOut', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({email: email, fcmToken: fcmToken})
-    });
-    if (response.status == 200) {
-      await AsyncStorage.removeItem('email').then(() => {
-      });
-      await AsyncStorage.removeItem('accessToken').then(() => {
-      });
-      navigation.replace('Auth');
-    } else {
-    }
-  }
+    try {
+      const response = await api.post('/auth/signOut', {email: email, fcmToken: fcmToken})
+      if (response.status == 200) {
+        await AsyncStorage.removeItem('email')
+        await AsyncStorage.removeItem('accessToken')
+        navigation.replace('Login');
+        navigation.navigate('Login');
+      };
+    } catch (error) {
+     console.error(error.response.status);
+    }; 
+  };
   return (
     <><View>
       <Text>MainScreen</Text>
@@ -36,7 +32,7 @@ const HomeMainScreen = ({navigation}) => {
         <Text>로그아웃</Text>
       </TouchableOpacity>
     </View><View>
-        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+        <TouchableOpacity onPress={() => navigation.replace('Auth')}>
           <Text>로그인 화면으로(임시)</Text>
         </TouchableOpacity>
       </View></>
